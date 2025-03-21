5e8b298f07a3eb2d55fa532ac58d94c3ca09310f
penki <penkiganesh2001@gmail.com>
2025-03-21 18:27:40 +0530

    fix

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ import cors from  'cors'
 const app = express()
 app.use(express.json())
 app.use(cors())
-const port =  3000
+const PORT = process.env.PORT || 3000;
 import  ticToeRoutes from './routes/tic-toe_routes.js';
 
 
 app.use( ticToeRoutes);
 
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
